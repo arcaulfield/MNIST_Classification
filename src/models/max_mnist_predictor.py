ec98ec7532b56395cbf14cc5713b15be0494c4cf
Alison Caulfield <arcaulfield@gmail.com>
2019-10-25 16:06:31 -0400

    implemented image processing into predict_max_num

--- src/models/max_mnist_predictor.py
+++ src/models/max_mnist_predictor.py
@@ -1,6 +1,6 @@
 from keras.models import Model
 import numpy as np
-
+from src.data_processing.number_extraction import extract_k_numbers
 
 class MaxMNISTPredictor:
     def __init__(self, model: Model):
@@ -8,13 +8,22 @@ class MaxMNISTPredictor:
 
     def predict_max_num(self, x):
         """
-        Predicts the number in each image in a n x 3 array and returns the maximum number in each row
-        :param x: a n x 3 x 784 x 1 array of images of individual numbers (each row contains the 3 numbers that are in 1 image)
+        predicts which number of three numbers in an array is the largest
+        :param x: a n x 128 x 128 numpy array of images
         :return: a n x 1 numpy array
         """
+
+        # isolate the 3 numbers in each image and save in test_x (n x 3 x 28 x 28 x 1 array)
+        test_x = np.empty((0, 3))
+        for i in range(x.shape(0)):
+            np.append(test_x, extract_k_numbers(x[i]), axis=0)
+        test_x = test_x.reshape((x.shape(0), 3, 28, 28, 1))
+
+
+        # predict the number in each of the three images and place the highest of the three in y
         predicted_y = np.empty(x.shape(0))
         for i in range(3):
-            y = np.argmax(self.model.predict(x[:, i]), axis=1)
+            y = np.argmax(self.model.predict(test_x[:, i]), axis=1)
             np.append(predicted_y, y, axis=1)
         predicted_y = np.amax(predicted_y, axis=1)
         return predicted_y
