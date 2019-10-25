5a209379c9d939152df820d1e50f21e61fa8b0a5
vaquierm <michaelvaquier74@gmail.com>
2019-10-25 00:32:15 -0400

    Figures generate for accuracy and loss

--- src/util/fileio.py
+++ src/util/fileio.py
@@ -92,6 +92,161 @@ def show_image(image: np.ndarray, title: str = ""):
     plt.show()
 
 
+def plot_training_history(history: dict):
+    """
+    Plot the validation and training accuracy/loss
+    :param history: keras model history
+    """
+    plt.plot(history['acc'])
+    plt.plot(history['val_acc'])
+    plt.title('Model accuracy')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc='upper left')
+    plt.show()
+
+    # Plot training & validation loss values
+    plt.plot(history['loss'])
+    plt.plot(history['val_loss'])
+    plt.title('Model loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc='upper left')
+    plt.show()
+
+
+def save_training_history(history: dict, acc_img_path: str, loss_img_path: str):
+    """
+    Save the validation and training accuracy/loss
+    :param history: keras model history
+    :param acc_img_path: File path to save accuracy image
+    :param loss_img_path: File path to save loss image
+    """
+    plt.plot(history['acc'])
+    plt.plot(history['val_acc'])
+    plt.title('Model accuracy')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc='upper left')
+    plt.savefig(acc_img_path)
+    plt.close()
+
+    # Plot training & validation loss values
+    plt.plot(history['loss'])
+    plt.plot(history['val_loss'])
+    plt.title('Model loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc='upper left')
+    plt.savefig(loss_img_path)
+
+
+def plot_confusion_matrix(cm: np.ndarray, classes,
+                          normalize=True,
+                          title=None,
+                          cmap=plt.cm.Blues):
+    """
+    Plots the configuration matrix
+    :param cm: Confusion matrix
+    :param classes: List of all classes
+    :param normalize: Normalizes the confusion matrix if true
+    :param title: Title of figure
+    :param cmap: Color map of confusion matrix
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix, without normalization'
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    plt.show()
+
+def save_confusion_matrix(cm: np.ndarray, classes,
+                          fig_file_path: str,
+                          normalize=True,
+                          title=None,
+                          cmap=plt.cm.Blues):
+    """
+    Saves the configuration matrix to file
+    :param cm: Confusion matrix
+    :param classes: List of all classes
+    :param fig_file_path: File path to save to
+    :param normalize: Normalizes the confusion matrix if true
+    :param title: Title of figure
+    :param cmap: Color map of confusion matrix
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if not os.path.isdir(os.path.dirname(fig_file_path)):
+        raise Exception("The directory", os.path.dirname(fig_file_path), "you are trying to save your confusion matrix does not exist")
+
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix, without normalization'
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    plt.savefig(fig_file_path)
+
+
 def save_model_weights(model_path: str, model: Model):
     """
     Saves the keras model weights to a file
